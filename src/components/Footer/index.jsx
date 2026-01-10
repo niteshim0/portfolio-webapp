d0cbc7eb6f1077d5ccb1014e2e0c73c21f865756
niteshim0 <niteshim111@gmail.com>
2026-01-10 13:02:18 +0530

    refactor

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -24,7 +24,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2024 Nitesh Kushwaha. All rights reserved.
+          &copy; 2026 Nitesh Kushwaha. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
